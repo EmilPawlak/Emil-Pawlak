86e645b9a9cc5223227e9369612abb815000b563
Emil Pawlak <Emil.Szymon.Pawlak@gmail.com>
2018-12-28 21:34:19 +0100

    Added further content, buttons and animations

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
 $('#fullpage').fullpage();
 });
+function showaboutme() {
+  aboutme.classList.add("animated", "fadeInLeft");
+  document.getElementById("aboutme").style.visibility = "visible";
+}
+function hideaboutme() {
+  aboutme.classList.remove("fadeInRight")
+  aboutme.classList.add("fadeOutLeft")
+}
+function showskills() {
+  skills.classList.add("animated", "fadeInRight");
+  document.getElementById("skills").style.visibility = "visible";
+}
+function hideskills() {
+  skills.classList.remove("fadeInLeft")
+  skills.classList.add("fadeOutRight")
+}
